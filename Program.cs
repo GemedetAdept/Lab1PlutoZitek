aacdf9c10dbf0f72f01cdbd1970afe4eb9be912e
GemedetAdept <vivian.zitek@gmail.com>
2022-09-12 14:06:44 -0600

    Complete Step 2: Order of Operations.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
-﻿void WorkWithIntegers(int inputX, int inputY) {
+﻿// Step 1: "Explore Integer Math"
+
+
+void WorkWithIntegers(int inputX, int inputY) {
 
 	Console.WriteLine("Input parameters:");
-	Console.WriteLine($"> x = {inputX}");
-	Console.WriteLine($"> y = {inputY}");
+		Console.WriteLine($"> x = {inputX}");
+		Console.WriteLine($"> y = {inputY}");
 
 	int combined = 0;
-		Console.WriteLine("Output value initalized.");
+		Console.WriteLine("Output variable initalized.");
 
 	combined = inputX + inputY; // Addition
 		Console.WriteLine($"> {inputX} + {inputY} = {combined}");
@@ -24,6 +27,7 @@
 
 int valueX = 0;
 int valueY = 0;
+int valueZ = 0;
 
 Console.WriteLine("Input value for X:");
 valueX = int.Parse(Console.ReadLine());
@@ -31,4 +35,37 @@ valueX = int.Parse(Console.ReadLine());
 Console.WriteLine("Input value for Y:");
 valueY = int.Parse(Console.ReadLine());
 
+Console.WriteLine("Input value for Z:");
+valueZ = int.Parse(Console.ReadLine());
+
 WorkWithIntegers(valueX, valueY);
+
+
+// Step 2: "Explore Order of Operations"
+
+
+void OrderOfOperations(int inputX, int inputY, int inputZ) {
+
+	Console.WriteLine("Input parameters:");
+		Console.WriteLine($"> x = {inputX}");
+		Console.WriteLine($"> y = {inputY}");
+		Console.WriteLine($"> z = {inputZ}");
+
+	int combined = 0;
+		Console.WriteLine("Output variable initalized.");
+
+	// Simple Order of Operations
+	combined = inputX + inputY * inputZ;
+		Console.WriteLine($"> {inputX} + {inputY} * {inputZ} = {combined}");
+
+	// Directed Order of Operations
+	combined = (inputX + inputY) * inputZ;
+		Console.WriteLine($"> ({inputX} + {inputY}) * {inputZ} = {combined}");
+
+	combined = (inputX - inputZ) / inputZ * inputY + (inputZ / inputY);
+		Console.WriteLine($"> ({inputX} - {inputZ}) / {inputZ} * {inputY} + ({inputZ / inputY}) = {combined}");
+
+}
+
+Console.WriteLine("\n");
+OrderOfOperations(valueX, valueY, valueZ);
