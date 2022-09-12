31e7c2df4ffc05b21b27e8c80e48fa69eed9a553
GemedetAdept <vivian.zitek@gmail.com>
2022-09-12 13:49:37 -0600

    Complete Step 1. Add user input functionality.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void WorkWithIntegers(int inputX, int inputY) {
+
+	Console.WriteLine("Input parameters:");
+	Console.WriteLine($"> x = {inputX}");
+	Console.WriteLine($"> y = {inputY}");
+
+	int combined = 0;
+		Console.WriteLine("Output value initalized.");
+
+	combined = inputX + inputY; // Addition
+		Console.WriteLine($"> {inputX} + {inputY} = {combined}");
+
+	combined = inputX - inputY; // Subtraction
+		Console.WriteLine($"> {inputX} - {inputY} = {combined}");
+
+	combined = inputX * inputY; // Multiplication
+		Console.WriteLine($"> {inputX} * {inputY} = {combined}");
+
+	combined = inputX / inputY; // Division
+		Console.WriteLine($"> {inputX} / {inputY} ≈ {combined}");
+
+}
+
+
+int valueX = 0;
+int valueY = 0;
+
+Console.WriteLine("Input value for X:");
+valueX = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input value for Y:");
+valueY = int.Parse(Console.ReadLine());
+
+WorkWithIntegers(valueX, valueY);
